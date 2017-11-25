ae379e260fd9b11b701cc2794a9d292a8bc6ead4
AntiTriki <ne.i.on@hotmail.com>
2017-11-25 10:44:47 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAjWnnhBU:APA91bHmlUhuz9gTS2ETdfTHIvagtyWpegI1pcgiuIDVWrkZQYWKQA4sdGoXvet0Ednxt_DBA47-BTi5t_h9XTDSeQCcnjJM13xzjUSacK2lNR8u2WjpdNi1fn5pQL1nk-v0Qm880u1N'
+};
